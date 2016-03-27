9a062a9131a06d2be497d18c1e8582a27634d785
chrismlusk <chris.m.lusk@gmail.com>
2016-03-27 17:13:17 -0400

    rewrite and update script

--- js/script.js
+++ js/script.js
@@ -1,39 +1,50 @@
-replaceText(document.body);
+findAndReplace(document.body);
 
-function replaceText(scope) {
-    var nodes;
-    var node;
-    if (scope.childNodes) {
-        nodes = scope.childNodes;
-        for (var i = 0; i < nodes.length; i++) {
-            node = nodes[i];
-            if (node.nodeType === 3) {
-                checkTextNode(node);
+function findAndReplace(node) 
+{
+    
+    var child, next;
+    
+    if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
+        || node.classList.indexOf('ace_editor') > -1) {
+        return;
+    }
+
+    switch ( node.nodeType )  
+    {
+        case 1:  // Element
+        case 9:  // Document
+        case 11: // Document fragment
+            child = node.firstChild;
+            while ( child ) 
+            {
+                next = child.nextSibling;
+                findAndReplace(child);
+                child = next;
             }
-            if (node.childNodes) {
-                replaceText(node);
-            }
-            node = null;
-        }
-        nodes = null;
+            break;
+
+        case 3: // Text node
+            handleText(node);
+            break;
     }
 }
 
-function checkTextNode(node) {
-    var newText = [
-        "Friendship Madness",
-        "FMAA",
-        "Friendly Four",
-        "Friendology"
-    ];
-    var oldText = [
-        /\bMarch Madness\b/gi,
-        /\bNCAA\b/g,
-        /\bFinal Four\b/g,
-        /\bBracketology\b/g
-    ];
-    for (var i = 0; i < oldText.length; i++) {
-        var text = node.data;
-        node.data = text.replace(oldText[i], newText[i]);
-    }
+function handleText(textNode) 
+{
+    var v = textNode.nodeValue;
+
+    v = v.replace(/\bMarch Madness\b/g, "Friendship Madness");
+    v = v.replace(/\bNCAA\b/g, "FMAA");
+    v = v.replace(/\bFinal Four\b/g, "Friendly Four");
+    v = v.replace(/\bthe Sooners\b/g, "the Calannis");
+    v = v.replace(/\bThe Sooners\b/g, "The Calannis");
+    v = v.replace(/\bthe Wildcats\b/g, "the Rutledges");
+    v = v.replace(/\bThe Wildcats\b/g, "The Rutledges");
+    v = v.replace(/\bthe Tar Heels\b/g, "the Lusks");
+    v = v.replace(/\bThe Tar Heels\b/g, "The Lusks");
+    v = v.replace(/\bthe Cavaliers\b/g, "the Lovetts");
+    v = v.replace(/\bThe Cavaliers\b/g, "The Lovetts");
+    
+    textNode.nodeValue = v;
 }
