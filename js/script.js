aee4cdea78148ba20d8c08fa5376db879899ae75
chrismlusk <chris.m.lusk@gmail.com>
2016-03-28 11:05:55 -0400

    update words for Friendly Four

--- js/script.js
+++ js/script.js
@@ -24,34 +24,42 @@ function handleText(textNode) {
         "FMAA",
         "Friendly Four",
         "Max Calanni",
+        "Calannis",
         "the Calannis",
         "The Calannis",
         "Katie Rutledge",
+        "Rutledges",
         "the Rutledges",
         "The Rutledges",
         "Chris Lusk",
+        "Lusks",
         "the Lusks",
         "The Lusks",
-        "Jake Lovett",
-        "the Lovetts",
-        "The Lovetts"
+        "Keifer Truett",
+        "Syracuse Truetts",
+        "the Truetts",
+        "The Truetts"
     ];
     var oldText = [
         /\bMarch Madness\b/gi,
         /\bNCAA\b/gi,
         /\bFinal Four\b/gi,
         /\bLon Kruger\b/gi,
+        /\bSooners\b/gi,
         /\bthe Sooners\b/gi,
         /\bThe Sooners\b/gi,
         /\bJay Wright\b/gi,
+        /\bWildcats\b/gi,
         /\bthe Wildcats\b/gi,
         /\bThe Wildcats\b/gi,
         /\bRoy Williams\b/gi,
+        /\bTar Heels\b/gi,
         /\bthe Tar Heels\b/gi,
         /\bThe Tar Heels\b/gi,
-        /\bTony Bennett\b/gi,
-        /\bthe Cavaliers\b/gi,
-        /\bThe Cavaliers\b/gi
+        /\bJim Boeheim\b/gi,
+        /\bSyracuse Orange\b/gi,
+        /\bthe Orange\b/gi,
+        /\bThe Orange\b/gi
     ];
     for (var i = 0; i < oldText.length; i++) {
         var text = textNode.nodeValue;
