86c5b132de70305371767e485a19e76c13efc4ab
Chris Lusk <chris.m.lusk@gmail.com>
2018-07-17 11:31:07 -0500

    lingering local changes

--- scripts.js
+++ scripts.js
@@ -1,32 +1,18 @@
-const TEXT_SWAP = [
-  [ /\bMarch Madness\b/g, "Friendship Madness" ],
-  [ /\bNCAA\b/g, "FMAA" ],
-  [ /\bFinal Four\b/g, "Friendly Four" ],
-  [ /\bOne Shining Moment\b/g, "One Friendly Moment" ],
-  [ /\bSelection Sunday\b/g, "Selection Monday" ],
-  [ /\bPresident Mark Emmert\b/g, "Comissioner James Corley" ],
-  [ /\bMark Emmert\b/g, "James Corley" ],
-  [ /\bEmmert\b/g, "Corley" ],
-  [ /\bMen's Basketball Committee\b/g, "Friendship Review Panel" ],
-  [ /\bbasketball championships\b/g, "friendship championships" ],
-  [ /\bNCAA champion\b/g, "James Corley's best friend" ],
-
-  // [ /\b\b/g, "" ],
-];
-
-let elements = document.getElementsByTagName("*");
-
-for (let i = 0; i < elements.length; i++) {
-  let element = elements[i];
-
-  for (let j = 0; j < element.childNodes.length; j++) {
-    let node = element.childNodes[j];
-
-    if (node.nodeType === 3) {
-      for (let k = 0; k < TEXT_SWAP.length; k++) {
-        let text = node.nodeValue;
-        node.nodeValue = text.replace(TEXT_SWAP[k][0], TEXT_SWAP[k][1]);
-      }
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  chrome.declarativeContent.onPageChanged.addRules([
+    {
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { schemes: ['https', 'http'] }
+        })
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
     }
-  }
-}
+  ]);
+});
+
+chrome.pageAction.onClicked.addListener(function() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.executeScript(tabs[0].id, { file: 'main.js' });
+  });
+});
